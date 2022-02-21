7b495be0ef9a8e46ff4ed10d2c40c83e57bba817
Joan <joan.domenech.masferrer@gmail.com>
2022-02-21 21:03:14 +0100

    method to list filenames (stem only)

--- file_manager.py
+++ file_manager.py
@@ -30,12 +30,25 @@ def list_all_filepaths_in_dir_with_specific_extension(directory, list_of_extensi
     return list_of_files
 
 
+def list_all_filenames_in_dir_with_specific_extension(directory, list_of_extensions):
+    list_of_filenames = []
+    for extension in list_of_extensions:
+        list_of_filenames += [f.stem for f in sorted(directory.glob(f"*.{extension}"))]
+    return list_of_filenames
+
+
 def list_all_pngs_in_dir(directory):
     return list_all_filepaths_in_dir_with_specific_extension(directory, ["png"])
 
 
 def list_all_image_filepaths_in_dir(directory):
     return list_all_filepaths_in_dir_with_specific_extension(
+        directory, ["png", "jpg", "jpeg", "bmp", "heic"]
+    )
+
+
+def list_all_image_filenames_in_dir(directory):
+    return list_all_filenames_in_dir_with_specific_extension(
         directory, ["png", "jpg", "jpeg", "bmp", "heic"]
     )
 
@@ -52,6 +65,6 @@ def load_json_file_content(path_to_json):
     return json.loads(json_content)
 
 
-def show_file(self, file_path):
+def show_file(file_path):
     new = 2  # open in a new tab, if possible
     webbrowser.open(f"file://{file_path}", new=new)
